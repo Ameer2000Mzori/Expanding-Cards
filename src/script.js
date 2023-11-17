e70e1c871575f6b85a1e48d5a8a0e5b603f0dba6
Ameer <ameerapex496@gmail.com>
2023-11-17 13:20:35 +0100

    added func to the project + images

--- src/script.js
+++ src/script.js
@@ -1,1 +1,16 @@
-console.log("hallo world");
+// getting our elements
+var imgWrapper = document.getElementsByClassName("img-Wrapper")[0];
+var panels = document.querySelectorAll(".panel");
+panels.forEach(function (panel) {
+    panel.addEventListener("click", function () {
+        removeActiveClasses();
+        panel.classList.add("active");
+    });
+});
+// our functions
+function removeActiveClasses() {
+    panels.forEach(function (panel) {
+        panel.classList.remove("active");
+    });
+}
+// our event lisnters
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,19 @@
-console.log("hallo world");
+// getting our elements
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel: Element) => {
+  // our event listeners
+  panel.addEventListener("click", () => {
+    // removing class from other panels
+    removeActiveClasses();
+    // adding class to our main panel
+    panel.classList.add("active");
+  });
+});
+
+// remove function
+function removeActiveClasses(): void {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
