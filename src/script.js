e7301c2bf596f31cb91426284ca816549fedf1c5
Ameer <ameerapex496@gmail.com>
2023-11-17 13:29:50 +0100

    added styling and func to the project

--- src/script.js
+++ src/script.js
@@ -1,16 +1,17 @@
 // getting our elements
-var imgWrapper = document.getElementsByClassName("img-Wrapper")[0];
 var panels = document.querySelectorAll(".panel");
 panels.forEach(function (panel) {
+    // our event listeners
     panel.addEventListener("click", function () {
+        // removing class from other panels
         removeActiveClasses();
+        // adding class to our main panel
         panel.classList.add("active");
     });
 });
-// our functions
+// remove function
 function removeActiveClasses() {
     panels.forEach(function (panel) {
         panel.classList.remove("active");
     });
 }
-// our event lisnters
